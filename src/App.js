e7b9096278ffdb85381feb232685e91b8a58c00e
Gregory Katchmar <gunk55@msn.com>
2019-02-06 05:39:58 -0800

    add material-ui-icons, prop-types & react-scroll-up; app.js - app.css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import UpIcon from "@material-ui-icons/ArrowUpward";
+import ScrollToTop from "react-scroll-up";
+import NavTabs from "./NavTabs";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const styles = theme => ({
+
+});
+
+function App(props) {
+  const classes = props.classes;
+  return (
+    <div>
+      <NavTabs />
+      <ScrollToTop showUnder={160}>
+      <Button fab color="primary" className={classes.button}>
+        <UpIcon/>
+      </Button>
+      </ScrollToTop>
+    </div>
+  );
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
