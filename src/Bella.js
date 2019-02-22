3cee66ed24cc518b17b85348b5bfc30566fa1ee0
Gregory Katchmar <gunk55@msn.com>
2019-02-22 10:41:50 -0800

    limit image gallery height

--- src/Bella.js
+++ src/Bella.js
@@ -1,6 +1,7 @@
 import ImageGallery from 'react-image-gallery';
 import React, {Component} from "react";
 import 'react-image-gallery/styles/css/image-gallery.css';
+import './bella.css';
 
 const PREFIX_URL = './images/bella/';
 
