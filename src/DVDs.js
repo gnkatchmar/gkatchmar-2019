ee6ef16d9b7261950b48c68bbb7487ea7b9ca4e1
Gregory Katchmar <gunk55@msn.com>
2022-09-15 11:24:18 -0700

    rests, update libib link

--- src/DVDs.js
+++ src/DVDs.js
@@ -10,7 +10,7 @@ class DVD extends Component {
   render() {
     return (
       <div style={divStyle}>
-        <iframe src="https://gnkatchmar.libib.com" title="dvd"></iframe>
+        <iframe src="https://libib.com/u/gnkatchmar" title="dvd"></iframe>
       </div>
     )
   }
